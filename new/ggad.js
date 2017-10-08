bd2feeb06ca83d4adc939e9471e3ddbe3a61ace0
xuxinxin <xuxinxin19941204@163.com>
2017-10-08 22:21:10 +0800

    js代码完成

---
+++ new/ggad.js
@@ -0,0 +1,14 @@
+document.writeln("<link type=\"text/css\" rel=\"stylesheet\" href=\"http://www.cssmoban.com/statics/css/astyle.css\" />");
+document.writeln("<div align=\"center\">");
+document.writeln("<script async src=\'//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\'></script>");
+document.writeln("<!-- 970-90 -->");
+document.writeln("<ins class=\'adsbygoogle\'");
+document.writeln("     style=\'display:inline-block;width:970px;height:90px\'");
+document.writeln("     data-ad-client=\'ca-pub-1542822386688301\'");
+document.writeln("     data-ad-slot=\'3747595052\'></ins>");
+document.writeln("<script>");
+document.writeln("(adsbygoogle = window.adsbygoogle || []).push({});");
+document.writeln("</script>");
+document.writeln("</div>");
+
+
